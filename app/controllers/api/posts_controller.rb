f7bb5c0046c8af832c4a30a89f25b43010bf0d65
Dan Kiss <flowfast47@gmail.com>
2018-05-14 09:38:30 -0700

    add create controller and route

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,5 +1,14 @@
 class Api::PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def show
     @post = Post.find(params[:id])
+  end
+  def create
+    @single_post = Post.create!(post_params)
+  end
+
+  private
+  def post_params
+    params.permit(:thought, :feeling, :body_sensation, :thought_rating, :feeling_rating, :body_sensation_rating, :hour_sleep, :hour_work, :hour_excercise, :hour_open, :hour_mediate, :acceptance, :text_body)
   end
 end
